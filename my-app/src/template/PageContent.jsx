eed1e70a35c83fe392e405744cf2a280056ad20e
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2023-02-20 13:01:53 +0300

    create site

--- my-app/src/template/PageContent.jsx
+++ my-app/src/template/PageContent.jsx
@@ -9,7 +9,7 @@ function PageContent() {
             <div className="w3-content w3-padding" style={{ maxWidth: 1564 }}>
                 <ProjectSection/>
                 <AboutSection/>
-                <Location/>
+                <Location/> 
             </div>
         </>
     )
